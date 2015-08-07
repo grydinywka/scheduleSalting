297bb07e1f6a43a70b5f5237e9d72dd31e21f40f
Sergiy Ignatenko <grydinywka@gmail.com>
2015-08-07 17:19:50 +0300

    create start hyper text page

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def salting_list(request):
+    return render(request, 'schedule/index.html', {})
+
