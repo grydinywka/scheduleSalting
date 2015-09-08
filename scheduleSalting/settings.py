0f9ffebc910be9d78c62e348f317e1670040f99d
Sergiy Ignatenko <grydinywka@gmail.com>
2015-09-08 23:05:30 +0300

    Transfer projrct on PostgreSQL database

--- scheduleSalting/settings.py
+++ scheduleSalting/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from django.conf import global_settings
+from db import DATABASES
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -83,14 +84,7 @@ PORTAL_URL = 'http://localhost:8000'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
-    }
-}
-
+# database in file db.py
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
