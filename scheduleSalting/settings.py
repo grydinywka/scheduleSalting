62dd1317894b7037479012ec13fa4fe840df482c
Sergiy Ignatenko <grydinywka@gmail.com>
2015-09-22 21:57:45 +0300

    create staticfiles

--- scheduleSalting/settings.py
+++ scheduleSalting/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
