4ce23f26bea2866223e78debc90ff3c0b8c1d342
Sergiy Ignatenko <grydinywka@gmail.com>
2015-09-22 22:03:17 +0300

    static ...

--- scheduleSalting/settings.py
+++ scheduleSalting/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
