46b54f8a29c8ede167c3eec8d72272c1b12da6fc
Sergiy Ignatenko <grydinywka@gmail.com>
2015-09-22 21:13:04 +0300

    attempt to enable static & bootstrap

--- scheduleSalting/settings.py
+++ scheduleSalting/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
