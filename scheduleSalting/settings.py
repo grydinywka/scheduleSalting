ee2180f341cf98f1bae0b64e40a5af5a159c6389
Sergiy Ignatenko <grydinywka@gmail.com>
2015-09-23 12:02:25 +0300

    try settings

--- scheduleSalting/settings.py
+++ scheduleSalting/settings.py
@@ -102,7 +102,7 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 # Enable Connection Pooling
-#DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
